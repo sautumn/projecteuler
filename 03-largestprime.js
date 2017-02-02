d527326bc880b9685485470b6e9fd9b3649a80ad
Autumn Smith <autumn07@gmail.com>
2017-02-01 16:06:26 -0800

    slow solution for primes

--- 03-largestprime.js
+++ 03-largestprime.js
@@ -2,48 +2,68 @@
 //
 // What is the largest prime factor of the number 600851475143 ?
 
-const largestPrime = (n) => {
-  // 0/1 are not prime numbers
-  // No prime number greater than 5 ends in a 5.
-  // Any number greater than 5 that ends in a 5 can be divided by 5.
-  // To prove whether a number is a prime number,
-  // first try dividing it by 2, and see if you get a whole number.
-  // If you do, it can't be a prime number.
-  // If you don't get a whole number, next try dividing it by
-  // prime numbers: 3, 5, 7, 11 (9 is divisible by 3)
-  // and so on, always dividing by a prime number (see table below).
+const findLargestPrimeLessThanN = (n) => {
   let primes = new Set();
-  // To prove whether a number is a prime number,
+  // 0/1 are not prime numbers
+  if (n === 0 || n === 1) {
+    return null;
+  }
   for (var i = 2; i <= n; i++) {
-    console.log('begin:', i);
-    let isPrime = false;
-    let flag = true;
+    let isPrime = true;
+    // To prove whether a number is a prime number,
     // first try dividing it by 2, and see if you get a whole number.
-    if (i % 2 !== 0 || i === 2) {
-      console.log('not div by 2 or is 2: ',i);
+    if (i % 2 !== 0) {
       // If you don't get a whole number, next try dividing it by
       // prime numbers: 3, 5, 7, 11 (9 is divisible by 3)
-      if (primes.size === 0 ) {
-        primes.add(i);
-      }
-      for (let item of primes){
-        console.log('check i against primes: ',i, 'prime being checked: ',item);
-
-        if (i % item === 0 || primes.size === 0) {
-          console.log('item', item)
-          console.log('is divisible by primes, ended');
-          flag = false;
+      if (primes.size > 0) {
+        for (let item of primes) {
+          if (i % item === 0) {
+            isPrime = false;
+            break;
+          }
         }
-        // break;
       }
-      if (!flag) {
-        console.log('not divisible by primes, added to primes', i);
+      if (isPrime) {
         primes.add(i);
       }
     }
   }
-  return primes.keys();
+  return Math.max(...primes.keys());
 };
 
-console.log(largestPrime(10));
-// console.log(largestPrime(600851475143));
+const findLargestPrimeofN = (n) => {
+  let primes = new Set(),
+      isPrime = true;
+  if (n <= 1) {
+    return 'No primes numbers below 2';
+  }
+  for (var i = 2; i <= n; i++) {
+    if (n % i === 0) {
+      if (primes.size > 0) {
+        for (let item of primes) {
+          if (i % item === 0) {
+            isPrime = false;
+            break;
+          }
+        }
+      }
+      if (isPrime) {
+        primes.add(i);
+      }
+    }
+  }
+  return Math.max(...primes.keys());
+};
+
+const assert = (test, expected, errorMessage) => {
+  if (test === expected) {
+    console.log(`Test passed. ${test} = ${expected}.`);
+    return 'Test passed';
+  }
+  console.log(`ERROR: ${errorMessage}`);
+  return;
+};
+
+assert(findLargestPrimeofN(13195), 29, 'Largest prime should = 29');
+// inefficient for large numbers
+// console.log(findLargestPrimeofN(600851475143));
